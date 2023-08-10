f621359b28cccf50d6d98ce1d1c7fecaefe28478
Suman Ghosh <ghoshsuman845@yahoo.com>
2023-08-10 14:42:43 +0530

    created a submission

--- script.js
+++ script.js
@@ -3,10 +3,23 @@ const arr = [1, 2, 3, 4, 1, 0, 2, 2];
 const sum = (arr) => arr.reduce((a, b) => a + b, 0);
 
 const divide = (arr, n) => {
-  const ind = arr.findIndex((e, i) => sum(arr.slice(0, i + 1)) > n);
-  return ind < 0 ? [arr] : [arr.slice(0, ind + 1), ...divide(arr.slice(ind + 1), n)];
+  let result = [];
+  let subarray = [];
+
+  for (let i = 0; i < arr.length; i++) {
+    subarray.push(arr[i]);
+    if (sum(subarray) > n) {
+      result.push(subarray.slice(0, -1));
+      subarray = [arr[i]];
+    }
+  }
+
+  if (subarray.length > 0) {
+    result.push(subarray);
+  }
+
+  return result;
 };
 
 const n = prompt("Enter n: ");
 alert(JSON.stringify(divide(arr, n)));
- 
