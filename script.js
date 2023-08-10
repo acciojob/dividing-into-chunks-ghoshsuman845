917e4a5ffd08520d95fef297d7d6a6fcf50cbbe6
Suman Ghosh <ghoshsuman845@yahoo.com>
2023-08-10 14:39:06 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,12 @@
 const arr = [1, 2, 3, 4, 1, 0, 2, 2];
 
-// your code here.
+const sum = (arr) => arr.reduce((a, b) => a + b, 0);
 
-// do not change the code below
+const divide = (arr, n) => {
+  const ind = arr.findIndex((e, i) => sum(arr.slice(0, i + 1)) > n);
+  return ind < 0 ? [arr] : [arr.slice(0, ind + 1), ...divide(arr.slice(ind + 1), n)];
+};
 
 const n = prompt("Enter n: ");
-alert(JSON.stringify(func(arr, n)));
+alert(JSON.stringify(divide(arr, n)));
+ 
